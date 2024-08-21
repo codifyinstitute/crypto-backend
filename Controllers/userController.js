5b58ae81d97a7098ff0c1e8767f484966ba9c326
Harshit Raul <115021736+HarshitRaul24@users.noreply.github.com>
2024-08-21 14:36:22 +0530

    Update userController.js

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -114,7 +114,7 @@ exports.verifyLoginOTP = async (req, res) => {
 
             await user.save();
 
-            res.status(200).json({ message: "Login successful" });
+            res.status(200).json({ message: "Login successful", token: user._id });
         } else {
             res.status(400).json({ message: "Invalid or expired OTP" });
         }
